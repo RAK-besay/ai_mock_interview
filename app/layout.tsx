9d75c7b36adb2a89e416fa900b2f6ba5de9b9588
RAK-besay <yphyo971@example.com>
2026-04-06 23:45:48 +0800

    setup app, file and folder structure, routing and auth page

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Mona_Sans, Geist } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Toaster } from "sonner";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Prep Wise",
+  description: "An AI-powered platform for preparing for mock interviews",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={cn("dark", "font-sans", geist.variable)}>
+      <body className={`${monaSans.className} antialiased pattern`}>
+          {children}
+
+            <Toaster/>
+      </body>
+    </html>
+  );
+}
